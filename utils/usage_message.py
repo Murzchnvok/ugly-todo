b43da770f9a0c4fe580a78ffa7b4df334202afc0
Pedro Araujo <murzchnvok@gmail.com>
2020-12-10 23:30:31 -0300

    updated

--- utils/usage_message.py
+++ utils/usage_message.py
@@ -12,44 +12,65 @@ USAGE_MESSAGE = f"""{attr(1)}see the examples below{attr(0)}
 {attr(1)}Show task(s) and note(s):{attr(0)}
 ~$ {attr(1)}{fg(2)}%(prog)s{attr(0)}
 
- {attr(4)}My Board{attr(0)} {fg(59)}[0/3]{attr(0)}
-   {fg(1)}{attr(0)}  {fg(59)}1. {fg(4)}{fg(7)}  develop a ugly To-Do app {fg(59)}@dev{attr(0)}
-   {fg(2)}{attr(0)}  {fg(59)}2. {fg(4)}{fg(7)}  Blader Runner {fg(59)}@read{attr(0)}
-   {fg(2)}{attr(0)}  {fg(59)}3. {fg(4)}{fg(7)}  python {fg(59)}@study{attr(0)}
+ {attr(1)}My Board {fg(59)}[1/6]{attr(0)}
 
- {attr(4)}Notes{attr(0)}
-   {fg(6)}{attr(0)}  {fg(59)}3. {fg(7)}just a reminder{attr(0)}
+   {attr(1)}To-Do{attr(0)}
+     {fg(2)}  {fg(59)}1. {fg(4)}  {fg(7)}Attack on Titan Vol. 7 {fg(59)}@read{attr(0)}
+     {fg(4)}  {fg(59)}3. {fg(4)}  {fg(7)}Blade Runner @read{attr(0)}
+     {fg(4)}  {fg(59)}6. {fg(4)}  {fg(7)}develop a fetch app {fg(59)}@dev{attr(0)}
+
+   {attr(1)}In Progress{attr(0)}
+     {fg(2)}  {fg(59)}2. {fg(4)}  {fg(7)}update polybar-collection {fg(59)}@dev{attr(0)}
+     {fg(1)}  {fg(59)}7. {fg(4)}  {fg(7)}update ugly-weather app {fg(59)}@dev{attr(0)}
+
+   {attr(1)}Done{attr(0)}
+     {fg(1)}  {fg(59)}4. {fg(2)}  {fg(59)}update utd to kanban style @dev{attr(0)}
+
+ Notes
+   {fg(6)}  {fg(59)}5. {fg(7)}remember to buy the milk{attr(0)}
 
 
 {attr(1)}Create a new task ('@' at the beginning of a word means a tag):{attr(0)}
 ~$ {attr(1)}{fg(2)}%(prog)s {fg(4)}-a {fg(6)}develop a ugly To-Do app @dev {fg(4)}-p {fg(6)}1{attr(0)}
 
- {attr(4)}My Board{attr(0)} {fg(59)}[0/1]{attr(0)}
-   {fg(1)}{attr(0)}  {fg(59)}1. {fg(4)}{fg(7)}  develop a ugly To-Do app {fg(59)}@dev{attr(0)}
+ {attr(1)}My Board {fg(59)}[0/2]{attr(0)}
+
+   {attr(1)}To-Do{attr(0)}
+     {fg(2)}  {fg(59)}1. {fg(4)}  {fg(7)}clean room {fg(59)}@home{attr(0)}
+     {fg(1)}  {fg(59)}2. {fg(4)}  {fg(7)}develop a ugly To-Do app {fg(59)}@dev{attr(0)}
 
 
-{attr(1)}Create a new note:{attr(0)}
-~$ {attr(1)}{fg(2)}%(prog)s {fg(4)}-n {fg(6)}just a simple note{attr(0)}
+{attr(1)}Start/stop a task:{attr(0)}
+~$ {attr(1)}{fg(2)}%(prog)s {fg(4)}-b {fg(6)}2{attr(0)}
 
- {attr(4)}Notes{attr(0)}
-   {fg(6)}{attr(0)}  {fg(59)}1. {fg(7)}just a simple note{attr(0)} 
+ {attr(1)}My Board {fg(59)}[0/2]{attr(0)}
+
+   {attr(1)}To-Do{attr(0)}
+     {fg(2)}  {fg(59)}1. {fg(4)}  {fg(7)}clean room {fg(59)}@home{attr(0)}
+
+   {attr(1)}In Progress{attr(0)}
+     {fg(1)}  {fg(59)}2. {fg(4)}  {fg(7)}develop a ugly To-Do app {fg(59)}@dev{attr(0)}
 
 
-{attr(1)}Check/uncheck task as complete (can check or uncheck multiples by id):{attr(0)}
-~$ {attr(1)}{fg(2)}%(prog)s {fg(4)}-c {fg(6)}2 3{attr(0)}
+{attr(1)}Check/uncheck task as complete:{attr(0)}
+~$ {attr(1)}{fg(2)}%(prog)s {fg(4)}-c {fg(6)}2{attr(0)}
 
- {attr(4)}My Board{attr(0)} {fg(59)}[2/3]{attr(0)}
-   {fg(1)}{attr(0)}  {fg(59)}1. {fg(4)}{fg(7)}  develop a ugly To-Do app {fg(59)}@dev{attr(0)}
-   {fg(2)}{attr(0)}  {fg(59)}2. {fg(2)}{fg(59)}  Blader Runner {fg(59)}@read{attr(0)}
-   {fg(2)}{attr(0)}  {fg(59)}3. {fg(2)}{fg(59)}  python {fg(59)}@study{attr(0)}
+ {attr(1)}My Board {fg(59)}[1/2]{attr(0)}
+
+   {attr(1)}To-Do{attr(0)}
+     {fg(2)}  {fg(59)}1. {fg(4)}  {fg(7)}clean room {fg(59)}@home{attr(0)}
+
+   {attr(1)}Done{attr(0)}
+     {fg(1)}  {fg(59)}2. {fg(2)}   {fg(59)}develop a ugly To-Do app @dev{attr(0)}
 
 
 {attr(1)}Delete a task/note (can delete multiples by id):{attr(0)}
-~$ {attr(1)}{fg(2)}%(prog)s {fg(4)}-d {fg(6)}1{attr(0)}
+~$ {attr(1)}{fg(2)}%(prog)s {fg(4)}-d {fg(6)}2{attr(0)}
 
- {attr(4)}My Board{attr(0)} {fg(59)}[2/2]{attr(0)}
-   {fg(2)}{attr(0)}  {fg(59)}2. {fg(2)}{fg(59)}  Blader Runner {fg(59)}@read{attr(0)}
-   {fg(2)}{attr(0)}  {fg(59)}3. {fg(2)}{fg(59)}  python {fg(59)}@study{attr(0)}
+ {attr(1)}My Board {fg(59)}[0/1]{attr(0)}
+
+   {attr(1)}To-Do{attr(0)}
+     {fg(2)}  {fg(59)}1. {fg(4)}  {fg(7)}clean room {fg(59)}@home{attr(0)}
 
 
 {attr(1)}Delete all completed tasks:{attr(0)}

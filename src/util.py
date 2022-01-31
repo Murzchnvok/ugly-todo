0204bf0d8fd41760b8536279abd884f1afa57642
Murzchnvok <murzchnvok@gmail.com>
2022-01-31 20:30:52 -0300

    first commit

---
+++ src/util.py
@@ -0,0 +1,20 @@
+from rich import print
+from rich.padding import Padding
+
+PaddingSize = tuple[int, int] | tuple[int, int, int, int]
+
+notify_type = {
+    "success": ("green", "s"),
+    "info": ("yellow", "i"),
+    "error": ("red", "x"),
+}
+
+
+def notify(message: str, type_: str = "success", padding: PaddingSize = (1, 1)) -> None:
+    color, icon_suffix = notify_type[type_]
+    type_ = f"[b {color}]:regional_indicator_symbol_letter_{icon_suffix}:"
+    print(Padding(f"{type_} {message}", padding))
+
+
+def list_to_str(list_: list[str]) -> str:
+    return " ".join(list_).strip()
